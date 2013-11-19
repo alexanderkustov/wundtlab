98d75fae22c61351cbe031e9836c69baf75e21d2
Alexander K <knuxus@gmail.com>
2013-11-19 14:00:21 +0000

    quase feito o load do bespoke

--- public/dist/preview_export/scripts/loadPresentation.js
+++ public/dist/preview_export/scripts/loadPresentation.js
@@ -1,7 +1,26 @@
 var loadPresentation = function() {
-	var presentation = localStorage.getItem('preview-string');
+	
+
+	var newURL = document.URL;
+	var apresentacao = newURL.split('#');
+	console.log(apresentacao[1]);
+
+	var presentation = apresentacao[1]; 
+	//need to get content of this file
+
+	// Check for the various File API support.
+		if (window.File && window.FileReader && window.FileList && window.Blob) {
+		  // Great success! All the File APIs are supported.
+		} else {
+		  alert('The File APIs are not fully supported in this browser.');
+		}
+
+
+
+	//var presentation = localStorage.getItem(apresentacao[1]);
+	//var presentation = localStorage.getItem('preview-string');
 	var config = JSON.parse(localStorage.getItem('preview-config'));
-	console.log('preview-string');
+	
 	if (presentation) {
 		document.body.innerHTML = presentation;
 	//	document.body.className = config.surface + " " + document.body.className;
