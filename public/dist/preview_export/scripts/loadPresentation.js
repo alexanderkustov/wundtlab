63fb491a2779564aac7b2a16ce1edde0a00c4f21
Alexander K <knuxus@gmail.com>
2013-11-20 11:10:55 +0000

    commit de ontem

--- public/dist/preview_export/scripts/loadPresentation.js
+++ public/dist/preview_export/scripts/loadPresentation.js
@@ -8,37 +8,19 @@ var loadPresentation = function() {
 	var presentation = apresentacao[1]; 
 	//need to get content of this file
 
-	// Check for the various File API support.
-		if (window.File && window.FileReader && window.FileList && window.Blob) {
-		  // Great success! All the File APIs are supported.
-		} else {
-		  alert('The File APIs are not fully supported in this browser.');
-		}
-
-
-	var xmlhttp;
-	
-		if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
-		    xmlhttp = new XMLHttpRequest();
-		}
-		else { // code for IE6, IE5
-		    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange = function() {
-		    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		        document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-		    }
-		}
-		xmlhttp.open("GET", "ajax_info.txt", true);
-		xmlhttp.send();
-
+	$.ajax({
+	    url : presentation,
+	    success : function(result){
+			console.log(result);
+	    }
+	});
 
 	//var presentation = localStorage.getItem(apresentacao[1]);
 	//var presentation = localStorage.getItem('preview-string');
 	var config = JSON.parse(localStorage.getItem('preview-config'));
 	
 	if (presentation) {
-		document.body.innerHTML = presentation;
+		document.body.innerHTML = result;
 	//	document.body.className = config.surface + " " + document.body.className;
 	}
 };
