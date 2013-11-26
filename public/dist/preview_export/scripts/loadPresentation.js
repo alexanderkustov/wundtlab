c7fbe220ade05ec77486df537755871823edb370
Alexander K <knuxus@gmail.com>
2013-11-26 13:29:02 +0000

    fucking progress

--- public/dist/preview_export/scripts/loadPresentation.js
+++ public/dist/preview_export/scripts/loadPresentation.js
@@ -1,47 +1,56 @@
+//ORIGNAL
+
+/*
+var loadPresentation = function() {
+        var presentation = localStorage.getItem('preview-string');
+        var config = JSON.parse(localStorage.getItem('preview-config'));
+
+        if (presentation) {
+                document.body.innerHTML = presentation;
+        //        document.body.className = config.surface + " " + document.body.className;
+        }
+};
+*/
+
 var loadPresentation = function() {
 
-var newURL = document.URL;
-var apresentacao = newURL.split('#');
+	var newURL = document.URL;
+	var apresentacao = newURL.split('#');
+
+	//caminho da apresentacao
+	//console.log(apresentacao[1]);
+
+	var presentation = apresentacao[1];
+	//need to get content of this file
+
+	var resultado = $.ajax({
+	url : presentation,
+		success : function(result){
+			async: false,
+			paraprocessar = result;
+
+		}
+	});
 
 
-//caminho da apresentacao
-console.log(apresentacao[1]);
+	$.when( resultado ).done(function(){
 
-var presentation = apresentacao[1];
-//need to get content of this file
+	var html = paraprocessar;
+	var init = html.indexOf("<body>");
+	var end = html.indexOf("</body>");
+	var conteudo = html.substring(init,end);
 
-var paraprocessar;
+	console.log("!!!SUBSTRING!!!!!!!!!");
+	console.log(conteudo);
+	console.log("!!!FINAL!!!!!!!!!");
+		document.body.innerHTML = conteudo;
+	});
 
-$.ajax({
-url : presentation,
-	success : function(result){
-	async: false,
-	console.log("!!!!RESULTADO!!!!!!!!!");
-	console.log(result);
-	paraprocessar = result;
-	console.log("!!!!FINAL!!!!!!!!!");
-}
-});
+	//var presentation = localStorage.getItem(apresentacao[1]);
+	//var presentation = localStorage.getItem('preview-string');
+	//var config = JSON.parse(localStorage.getItem('preview-config'));
+ 
 
+	//	document.body.className = config.surface + " " + document.body.className;
 
-console.log("!!!!AJAX!!!!!!!!!");
-
-
-var html = paraprocessar;
-var init = html.indexOf("<body>");
-var end = html.indexOf("</body>");
-var body = html.substring(init,end);
-
-console.log("!!!SUBSTRING!!!!!!!!!");
-
-console.log(body);
-
-//var presentation = localStorage.getItem(apresentacao[1]);
-//var presentation = localStorage.getItem('preview-string');
-var config = JSON.parse(localStorage.getItem('preview-config'));
-
-if (presentation) {
-document.body.innerHTML = body;
-//	document.body.className = config.surface + " " + document.body.className;
-}
 };
